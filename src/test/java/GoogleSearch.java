f1486c3b79e9fc8bb34bb6da9b37ab47cd95be67
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-04-03 21:57:37 -0300

    Update

---
+++ src/test/java/GoogleSearch.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class GoogleSearch {
+
+    @Test
+    public void searchAdaTech() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(
+                new ChromeOptions().addArguments("--remote-allow-origins=*")
+        );
+        try {
+            driver.get("https://www.google.com");
+            driver.findElement(By.cssSelector("[name='q'")).sendKeys("Ada tech");
+
+            
+        } finally {
+            driver.quit();
+        }
+    }
+}
