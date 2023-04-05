7090e4858749825a07c147d626fd3fd541db6164
William Cesar Santos <william_cesar_santos@hotmail.com>
2023-04-05 19:44:11 -0300

    Update

--- src/test/java/GoogleSearch.java
+++ src/test/java/GoogleSearch.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class GoogleSearch {
 
@@ -15,11 +19,15 @@ public class GoogleSearch {
         );
         try {
             driver.get("https://www.google.com");
-            driver.findElement(By.cssSelector("[name='q'")).sendKeys("Ada tech");
+            driver.findElement(By.cssSelector("[name='q']")).sendKeys("Ada tech");
 
-
+            new WebDriverWait(driver, Duration.ofSeconds(20))
+                    .until(
+                            ExpectedConditions.elementToBeClickable(By.cssSelector("[name='btnK']"))
+                    ).click();
         } finally {
             driver.quit();
         }
     }
+
 }
